a96197b7b2022c0a366645fb38584befa8081cfc
Vinicius <104523463+vinicim002@users.noreply.github.com>
2024-06-30 20:30:53 -0300

    cirando o arquivo produto.index - ok

--- menu/urls.py
+++ menu/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path("categoria/<int:id>", views.categorias, name='categoria'),
+    path("itemMenu/<int:id>", views.itemMenu, name='produto'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from usuarios.models import Usuario
 from .models import ItemMenu, Categoria
+from django.http import HttpResponse
 
 def home(request):
     if request.session.get('usuario'):
@@ -15,7 +16,6 @@ def home(request):
         if not request.session.get('carrinho'):
             request.session['carrinho'] = []
             request.session.save()
-
         itemMenus = ItemMenu.objects.filter(disponivel=True)  # Filtrar apenas os itens disponíveis
         categorias = Categoria.objects.all()
 
@@ -24,3 +24,27 @@ def home(request):
             'carrinho': len(request.session['carrinho']),
             'categorias': categorias,
         })
+
+
+def categorias(request, id):
+    if not request.session.get('carrinho'):
+        request.session['carrinho'] = []
+        request.session.save()
+    itemMenus = ItemMenu.objects.filter(categoria_id = id)
+    categorias = Categoria.objects.all()
+
+    return render(request, 'home.html', {'itemMenus': itemMenus,
+                                        'carrinho': len(request.session['carrinho']),
+                                        'categorias': categorias,})
+
+def itemMenu(request, id):
+    if not request.session.get('carrinho'):
+        request.session['carrinho'] = []
+        request.session.save()
+    erro = request.GET.get('erro')
+    itemMenu = ItemMenu.objects.filter(id=id)[0]
+    categorias = Categoria.objects.all()
+    return render(request, 'produto.html', {'itemMenu': itemMenu, 
+                                            'carrinho': len(request.session['carrinho']),
+                                            'categorias': categorias,
+                                            'erro': erro})
