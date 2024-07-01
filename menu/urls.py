a06788509b76092037b3b4e98c21a70ab52ae6a8
Vinicius <104523463+vinicim002@users.noreply.github.com>
2024-06-30 23:17:24 -0300

    Algumas altercoes - incerto

--- menu/urls.py
+++ menu/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path("categoria/<int:id>", views.categorias, name='categoria'),
     path("itemMenu/<int:id>", views.itemMenu, name='produto'),
+    path("add_carrinho", views.add_carrinho, name='add_carrinho'),
 ]
